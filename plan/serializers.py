da93f19fac0945275bb288899f937af15bc61d2a
lnxpy <lnxpylnxpy@gmail.com>
2020-05-02 17:56:15 +0430

    user serializer modified

--- plan/serializers.py
+++ plan/serializers.py
@@ -12,7 +12,7 @@ class ToDoSerializer(serializers.ModelSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('username', 'avatar')
+        fields = ('username', 'avatar', 'name')
 
 
 class ThemeSerializer(serializers.ModelSerializer):
@@ -24,4 +24,11 @@ class ThemeSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        print(validated_data)
+        user = User.objects.create_user(**validated_data)
+        Profile.objects.create(username=user)
+        return user
