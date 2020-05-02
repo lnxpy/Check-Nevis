0f35fe264de405268ab1aa8feaa665f7392dfa82
lnxpy <lnxpylnxpy@gmail.com>
2020-05-02 10:43:10 +0430

    all adds are avaliable for local server

--- checknevis/settings.py
+++ checknevis/settings.py
@@ -26,7 +26,7 @@ with open(os.path.join(BASE_DIR, 'checknevis/secret.txt')) as sec:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
