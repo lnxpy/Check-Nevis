ea6b7443f233abb3fbf5ac8f4473a14896b3a645
lnxpy <lnxpylnxpy@gmail.com>
2020-05-04 14:11:22 +0430

    details urls + classes added

--- plan/urls.py
+++ plan/urls.py
@@ -5,11 +5,18 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 
-    path('api/todo', views.ToDoAPIView.as_view(), name='todo'),
-    path('api/profile', views.ProfileAPIView.as_view(), name='profile'),
-    path('api/theme', views.ThemeAPIView.as_view(), name='theme'),
-    path('api/user', views.UserAPIView.as_view(), name='user'),
+    path('api/todo/', views.ToDoAPIView.as_view(), name='todo'),
+    path('api/todo/<int:pk>/', views.ToDoDetailAPIView.as_view(), name='todo_detail'),
 
-    path('api/login', include('rest_framework.urls')),
+    path('api/profile/', views.ProfileAPIView.as_view(), name='profile'),
+    path('api/profile/<int:pk>/',
+         views.ProfileDetailAPIView.as_view(), name='profile_detail'),
+
+    path('api/theme/', views.ThemeAPIView.as_view(), name='theme'),
+    path('api/theme/<int:pk>/',
+         views.ThemeDetailAPIView.as_view(), name='theme_detail'),
+
+    path('api/user/', views.UserAPIView.as_view(), name='user'),
+    path('api/login/', include('rest_framework.urls')),
     path('api/', include('rest_auth.urls')),
 ]
--- plan/views.py
+++ plan/views.py
@@ -3,7 +3,9 @@ from . import models
 from rest_framework import generics
 from .serializers import ToDoSerializer, ProfileSerializer, UserSerializer, ThemeSerializer
 from django.contrib.auth.models import User
+from .permissions import AnybodyIsAble
 
+from rest_framework.response import Response
 from django.http import HttpResponse
 
 
@@ -20,17 +22,65 @@ class ToDoAPIView(generics.ListCreateAPIView):
     queryset = models.ToDo.objects.all()
     serializer_class = ToDoSerializer
 
+    def get(self, request, format=None):
+        ToDos = models.ToDo.objects.filter(author=request.user.id)
+        serializer = ToDoSerializer(ToDos, many=True)
+        return Response(serializer.data)
+
+
+class ToDoDetailAPIView(generics.RetrieveUpdateAPIView):
+    queryset = models.ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+    def get(self, request, pk, format=None):
+        ToDos = models.ToDo.objects.filter(author=request.user.id)
+        filtered = ToDos.filter(id=pk)
+        serializer = ToDoSerializer(filtered, many=True)
+        return Response(serializer.data)
+
 
 class ProfileAPIView(generics.ListCreateAPIView):
     queryset = models.Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get(self, request, format=None):
+        Profile = models.Profile.objects.filter(username=request.user.id)
+        serializer = ProfileSerializer(Profile, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetailAPIView(generics.ListCreateAPIView):
+    queryset = models.Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get(self, request, pk, format=None):
+        Profile = models.Profile.objects.filter(username=request.user.id)
+        filtered = Profile.filter(id=pk)
+        serializer = ProfileSerializer(filtered, many=True)
+        return Response(serializer.data)
 
 
 class ThemeAPIView(generics.ListCreateAPIView):
     queryset = models.Theme.objects.all()
     serializer_class = ThemeSerializer
 
+    def get(self, request, pk, format=None):
+        Theme = models.Theme.objects.filter(username=request.user.id)
+        serializer = ThemeSerializer(Theme, many=True)
+        return Response(serializer.data)
+
+
+class ThemeDetailAPIView(generics.RetrieveUpdateAPIView):
+    queryset = models.Theme.objects.all()
+    serializer_class = ThemeSerializer
+
+    def get(self, request, format=None):
+        Theme = models.Theme.objects.filter(username=request.user.id)
+        serializer = ThemeSerializer(Theme, many=True)
+        return Response(serializer.data)
+
 
 class UserAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (AnybodyIsAble,)
