e7fbf93f17b91156ea9c02c4ff93cb8f49af771e
lnxpy <lnxpylnxpy@gmail.com>
2020-05-06 11:38:34 +0430

    loged in users are not permitted

--- plan/permissions.py
+++ plan/permissions.py
@@ -4,4 +4,6 @@ from rest_framework import permissions
 class AnybodyIsAble(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return False
         return True
