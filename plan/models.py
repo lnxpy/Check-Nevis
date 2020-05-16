8d70447a12dffa02e82dd6ee3114c21644369d67
lnxpy <lnxpylnxpy@gmail.com>
2020-05-16 10:20:34 +0430

    done field + django resize added

--- plan/models.py
+++ plan/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
+from django_resized import ResizedImageField
 
 
 class Profile(models.Model):
 
     username = models.OneToOneField(User, on_delete=models.DO_NOTHING)
-    avatar = models.ImageField(upload_to='avatars', blank=True)
+    #avatar = models.ImageField(upload_to='avatars', blank=True)
+    avatar = ResizedImageField(
+        size=[500, 500], crop=['middle', 'center'], upload_to='avatars', blank=True, null=True)
     name = models.CharField(max_length=20)
 
     def __str__(self):
@@ -19,6 +22,7 @@ class ToDo(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date = jmodels.jDateTimeField(auto_now=True, blank=False)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return '%s - %s' % (self.author, self.title)
